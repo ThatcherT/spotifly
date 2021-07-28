3c58d8815dd66c4991dce48db5f5455a4a4e5b6c
thatchert <thatch@utexas.edu>
2021-07-28 00:25:12 -0500

    g

--- music/music/settings.py
+++ music/music/settings.py
@@ -92,7 +92,7 @@ else:
     import dj_database_url
     DATABASES = {
         'default': dj_database_url.config(
-            conn_max_age=600, ssl_required=True
+            conn_max_age=600, ssl_require=True
         )
     }
 

1404130946bcc892422b7547e19d857bab5901d0
thatchert <thatch@utexas.edu>
2021-07-28 00:20:37 -0500

    f

--- music/music/asgi.py
+++ music/music/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'music.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'music.music.settings')
 
 application = get_asgi_application()

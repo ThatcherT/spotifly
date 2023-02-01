9b68e2f9b84adc9cea35187ae063699a3d166335
Thatcher Thornberry <thatcher.thornberry@bpx.com>
2023-02-01 15:02:19 -0700

    add react stuff

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          QSONGS X REACT LETS GO
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
